c0638f6411e6ff0d7e777fe8ac773fbc94890fbe
Lucien <61230075+liruchen32@users.noreply.github.com>
2022-09-20 16:02:47 +0800

    init(Footer): create project and Footer

---
+++ components/Footer/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-footer class="d-flex flex-column">
+    <v-row class="ma-0 pa-0">
+      <v-col cols="12" sm="3">
+        <div class="ma-auto pa-auto text-center">
+          <h2>CONTACT</h2>
+          <br />
+          <h5>
+            <v-icon>{{ mdiMapMarker }}</v-icon>
+            台北市大安區
+          </h5>
+          <h5>大安路一段31巷50號</h5>
+          <br />
+          <h5>
+            <v-icon>{{ mdiPhone }}</v-icon>
+            02-87738077
+          </h5>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="3">
+        <div class="ma-auto pa-auto text-center">
+          <h2>OPENING HOURS</h2>
+          <br />
+          <h5>Sun to Mon</h5>
+          <h6>6PM - 2AM</h6>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <h2 class="text-center">FIND US</h2>
+        <div class="px-5">
+          <v-card width="100%">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.790625209163!2d121.54248085056375!3d25.041178583890346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abf926bee7db%3A0xf2458452829c2195!2zQkFS5a6uLU1peWE!5e0!3m2!1szh-TW!2stw!4v1598167839490!5m2!1szh-TW!2stw"
+              frameborder="0"
+              width="100%"
+              style="border: 0"
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0"
+            ></iframe>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row justify="center" no-gutters>
+      <v-col>
+        <v-btn
+          v-for="button in icons"
+          :key="button.link"
+          class="ma-4 black--text"
+          icon
+          :href="button.link"
+          target="_blank"
+        >
+          <v-icon size="36px">{{ button.icon }}</v-icon>
+        </v-btn>
+        <v-col class="py-3 text-center black--text" cols="12">
+          <strong>{{ new Date().getFullYear() }} — BAR宮</strong>
+        </v-col>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup>
+import { mdiFacebook, mdiInstagram, mdiPhone, mdiMapMarker } from '@mdi/js';
+const icons = [
+  {
+    icon: mdiFacebook,
+    link: 'https://www.facebook.com/pages/category/Bar/BAR%E5%AE%AE-Miya-337530157118054/'
+  },
+  {
+    icon: mdiInstagram,
+    link: 'https://www.instagram.com/miya_taipei/?hl=zh-tw'
+  }
+];
+</script>
