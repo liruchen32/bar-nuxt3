983a809cf9515d41e3b83e784e7a058ba61896ed
Lucien <61230075+liruchen32@users.noreply.github.com>
2022-09-26 02:50:54 +0800

    feat(CocktailCard): CocktailCard component

--- components/CocktailCard/index.vue
+++ components/CocktailCard/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="d-flex justify-center">
-    <v-hover v-slot="{ isHovering, props }">
-      <v-img
-        class="rounded-xl"
-        :class="isHovering ? 'elevation-12' : 'elevation-2'"
-        width="300px"
-        v-bind="props"
-        :src="`/cocktails/${imgSrc}.jpg`"
-      />
-    </v-hover>
+  <div
+    class="card ma-auto"
+    :class="isHover ? 'elevation-12' : 'elevation-2'"
+    @mouseenter="isHover = true"
+    @mouseleave="isHover = false"
+  >
+    {{ isHover }}
+    <div class="card_face card_face--front">
+      <img :src="`/cocktails/${frontImgSrc}.jpg`" />
+    </div>
+    <div class="card_face card_face--back">
+      <img :src="`/cocktails/${backImgSrc}.jpg`" />
+    </div>
   </div>
 </template>
 
@@ -17,14 +20,58 @@ export default {
   name: 'Cocktail-Card',
 
   props: {
-    imgSrc: {
+    frontImgSrc: {
+      type: String
+    },
+    backImgSrc: {
       type: String
     }
   },
 
   setup(props) {
-    const { imgSrc } = toRefs(props);
-    return { imgSrc };
+    const isHover = ref(false);
+    const { frontImgSrc, backImgSrc } = toRefs(props);
+    return { isHover, frontImgSrc, backImgSrc };
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.card {
+  position: relative;
+  width: 300px;
+  height: 500px;
+  cursor: pointer;
+  transition: 1s ease-in-out;
+  transform-style: preserve-3d;
+  border-radius: 12px;
+
+  &:hover {
+    transform: rotateY(0.5turn);
+  }
+
+  .card_face {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    transition: 1s ease-in-out;
+
+    // -webkit-box-reflect: below 0
+    //   linear-gradient(transparent, transparent, rgba(0, 0, 0, 0.4));
+
+    img {
+      width: 300px;
+      height: 500px;
+      object-fit: cover;
+      border-radius: 12px;
+    }
+
+    &--back {
+      transform: rotateY(0.5turn);
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,14 @@
         </div>
       </Transition>
     </v-parallax>
+    <v-row class="d-flex ma-4">
+      <v-col cols="12" sm="6" md="4" v-for="n in cocktailImages" :key="n">
+        <CocktailCard
+          :front-img-src="`${n}_front`"
+          :back-img-src="`${n}_back`"
+        />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -21,6 +29,11 @@
 export default {
   setup() {
     const render = ref(false);
+    const cocktailImages = ref([
+      '001_FLAVORED_GIN_TONIC',
+      '002_NENE',
+      '003_JUGURONI'
+    ]);
 
     onMounted(() => {
       setTimeout(() => {
@@ -28,7 +41,7 @@ export default {
       }, 500);
     });
 
-    return { render };
+    return { render, cocktailImages };
   }
 };
 </script>
