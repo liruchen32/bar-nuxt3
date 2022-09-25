05d66aa7bb4f3a463995f5d7af47a7d848001be2
Lucien <61230075+liruchen32@users.noreply.github.com>
2022-09-25 20:09:31 +0800

    feat(pages/index): add slide y transition

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,15 @@
       src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
       height="100vh"
     >
-      <div
-        class="h-100 d-flex flex-column justify-center align-center"
-        v-if="render"
-      >
-        <h1 class="text-h3 mb-4">BAR 宮</h1>
-        <h4 class="text-h6">我有酒，你有故事嗎？</h4>
-      </div>
+      <Transition name="slide-y">
+        <div
+          class="h-100 d-flex flex-column justify-center align-center"
+          v-if="render"
+        >
+          <h1 class="text-h3 mb-4">BAR 宮</h1>
+          <h4 class="text-h6">我有酒，你有故事嗎？</h4>
+        </div>
+      </Transition>
     </v-parallax>
   </div>
 </template>
@@ -33,3 +35,21 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.slide-y-enter-active {
+  transition: all 0.8s ease-out;
+}
+
+.slide-y-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-y-enter-from {
+  transform: translateY(100vh);
+}
+.slide-y-leave-to {
+  transform: translateY(0);
+  opacity: 0;
+}
+</style>
