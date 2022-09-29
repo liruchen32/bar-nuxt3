84518e997677d9334e01690e9e59a4f9b23ddbe8
Lucien <61230075+liruchen32@users.noreply.github.com>
2022-09-29 19:44:26 +0800

    feat(app.vue): add head meta

--- app.vue
+++ app.vue
@@ -5,3 +5,12 @@
     </NuxtLayout>
   </div>
 </template>
+<script setup>
+useHead({
+  title: 'BAR 宮',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [{ name: 'description', content: 'BAR 宮' }],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+});
+</script>
