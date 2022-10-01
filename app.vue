ba821790390c3959766d902d8f9488d57aff59a9
Lucien <61230075+liruchen32@users.noreply.github.com>
2022-10-01 16:16:28 +0800

    feat(): add home page logo and head description

--- app.vue
+++ app.vue
@@ -10,7 +10,9 @@ useHead({
   title: 'BAR 宮',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   charset: 'utf-8',
-  meta: [{ name: 'description', content: 'BAR 宮' }],
+  meta: [
+    { name: 'description', content: 'BAR 宮 | 台北市大安區大安路一段31巷50號' }
+  ],
   link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
 });
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="ma-0 pa-0">
-    <v-parallax src="/home_page_bg.png" height="100vh">
+    <v-parallax src="/home_page_bg.png" class="parallax_body">
       <Transition name="slide-y">
         <div
           class="h-100 d-flex flex-column justify-center align-center"
           v-if="render"
         >
-          <h1 class="text-h3 mb-4">BAR 宮</h1>
-          <h4 class="text-h6">我有酒，你有故事嗎？</h4>
+          <div style="width: 40%">
+            <v-img src="logos/logo.png"></v-img>
+          </div>
+          <h1 class="text-h3 my-2 text-white">BAR 宮</h1>
+          <h4 class="text-h6 text-white">我有酒，你有故事嗎？</h4>
         </div>
       </Transition>
     </v-parallax>
@@ -46,5 +49,15 @@ export default {
 .slide-y-leave-to {
   transform: translateY(0);
   opacity: 0;
+}
+
+.parallax_body {
+  height: 100vh;
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(255, 255, 255, 1) 100%,
+    rgba(255, 255, 255, 1) 100%
+  );
 }
 </style>
